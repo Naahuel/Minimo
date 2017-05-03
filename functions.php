5a3be8e91d7f320c2065311dcc5e5f90a22ee8d7
Nahuel José <nahuel@abracreativa.com>
2017-05-03 00:07:17 -0300

    General structure; Typography

--- functions.php
+++ functions.php
@@ -105,11 +105,13 @@ add_action( 'widgets_init', 'minimo_widgets_init' );
  * Enqueue scripts and styles.
  */
 function minimo_scripts() {
+	// Google Fonts
+	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Halant:600|Merriweather:400,400i,700,700i' );
+	// Tye styles
 	wp_enqueue_style( 'minimo-style', get_stylesheet_uri() );
 
+	// Scripts
 	wp_enqueue_script( 'minimo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'minimo-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
