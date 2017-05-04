4ec51505b0d9722dfea8e11797e285fc1815cc00
Nahuel José <nahuel@abracreativa.com>
2017-05-04 02:12:20 -0300

    Minor adjustments in typography, layout and content

--- header.php
+++ header.php
@@ -24,18 +24,13 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'minimo' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<?php the_header_image_tag(); ?>
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
 		</div><!-- .site-branding -->
--- template-parts/content.php
+++ template-parts/content.php
@@ -13,8 +13,10 @@
 	<header class="entry-header">
 		<?php
 		if ( is_single() ) :
+			the_post_thumbnail('full');
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
+			the_post_thumbnail('thumbnail');
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
